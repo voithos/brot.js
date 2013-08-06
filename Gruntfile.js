e30e032353c5ffbab285f36ff731377669cf5a14
Zaven Muradyan <megalivoithos@gmail.com>
2013-08-06 14:56:07 -0700

    Add force options to grunt tasks and add watchtest task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,95 +1,118 @@
-'use strict';
+(function() {
+  'use strict';
 
-module.exports = function(grunt) {
+  module.exports = function(grunt) {
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    jshint: {
-      options: {
-        newcap: false
+    grunt.initConfig({
+      pkg: grunt.file.readJSON('package.json'),
+      jshint: {
+        options: {
+          newcap: false
+        },
+        all: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js']
       },
-      all: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js']
-    },
-    cafemocha: {
-      all: {
-        src: 'test/**/*.js',
-        options: {
-          ui: 'bdd',
-          reporter: 'list'
+      cafemocha: {
+        all: {
+          src: 'test/**/*.js',
+          options: {
+            ui: 'bdd',
+            reporter: 'list'
+          }
         }
-      }
-    },
-    browserify: {
-      'build/brot.js': 'src/brot.js'
-    },
-    watch: {
-      dev: {
-        files: ['src/**/*.js', '!src/textbrot.js'],
-        tasks: ['build'],
-        options: {
-          livereload: true
-        }
-      }
-    },
-    connect: {
-      options: {
-        port: 8008,
-        hostname: 'localhost',
-        base: '.'
       },
-      dev: {
+      browserify: {
+        'build/brot.js': 'src/brot.js'
+      },
+      watch: {
+        dev: {
+          files: ['src/**/*.js', '!src/textbrot.js'],
+          tasks: ['usetheforce_on', 'test', 'build', 'usetheforce_off'],
+          options: {
+            livereload: true
+          }
+        },
+        test: {
+          files: ['src/**/*.js', 'test/**/*.js', 'Gruntfile.js'],
+          tasks: ['usetheforce_on', 'test', 'usetheforce_off']
+        }
+      },
+      connect: {
         options: {
-          middleware: function(connect, options) {
-            return [
-              require('connect-livereload')(),
-              connect.static(options.base)
-            ];
+          port: 8008,
+          hostname: 'localhost',
+          base: '.'
+        },
+        dev: {
+          options: {
+            middleware: function(connect, options) {
+              return [
+                require('connect-livereload')(),
+                connect.static(options.base)
+              ];
+            }
+          }
+        }
+      },
+      open: {
+        dev: {
+          path: 'http://localhost:8008'
+        }
+      },
+      uglify: {
+        options: {
+          banner: '/*! <%= pkg.name %> - v<%= pkg.version %> */\n'
+        },
+        nomin: {
+          options: {
+            mangle: false,
+            compress: false,
+            beautify: true
+          },
+          files: {
+            'build/brot.js': ['build/brot.js']
+          }
+        },
+        min: {
+          files: {
+            'build/brot.min.js': ['build/brot.js']
           }
         }
       }
-    },
-    open: {
-      dev: {
-        path: 'http://localhost:8008'
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-cafe-mocha');
+    grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-open');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    // Workaround to force continuation when encountering errors
+    // during development cycle (for watch / livereload)
+    grunt.registerTask('usetheforce_on', '// force the force option', function() {
+      if (!grunt.option('force')) {
+        grunt.config.set('usetheforce', true);
+        grunt.option('force', true);
       }
-    },
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> - v<%= pkg.version %> */\n'
-      },
-      nomin: {
-        options: {
-          mangle: false,
-          compress: false,
-          beautify: true
-        },
-        files: {
-          'build/brot.js': ['build/brot.js']
-        }
-      },
-      min: {
-        files: {
-          'build/brot.min.js': ['build/brot.js']
-        }
+    });
+    grunt.registerTask('usetheforce_off', '// remove the force option', function() {
+      if (grunt.config.get('usetheforce')) {
+        grunt.option('force', false);
       }
-    }
-  });
+    });
 
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-cafe-mocha');
-  grunt.loadNpmTasks('grunt-browserify');
-  grunt.loadNpmTasks('grunt-open');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.registerTask('develop', 'Setup development server and watch files',
+                       ['usetheforce_on', 'test', 'build',
+                        'connect', 'open', 'watch:dev', 'usetheforce_off']);
+    grunt.registerTask('test', 'Lint and test source files',
+                       ['jshint', 'cafemocha']);
+    grunt.registerTask('watchtest', 'Watch for changes and lint and test source files',
+                       ['watch:test']);
+    grunt.registerTask('build', 'Combine and compress source for the frontend',
+                       ['browserify', 'uglify']);
 
-  grunt.registerTask('develop', 'Setup development server and watch files',
-                     ['build', 'connect', 'open', 'watch']);
-  grunt.registerTask('test', 'Lint and test source files',
-                     ['jshint', 'cafemocha']);
-  grunt.registerTask('build', 'Combine and compress source for the frontend',
-                     ['browserify', 'uglify']);
+    grunt.registerTask('default', ['develop']);
 
-  grunt.registerTask('default', ['develop']);
-
-};
+  };
+})();
